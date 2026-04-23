16c80b93f1a2fb0af922595362f368b323bc5fb3
Brayan Garcia <mfna24brayan@gmail.com>
2026-04-23 12:39:23 -0500

    arreglo de env y app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
+import { WorkerModule } from './worker/worker.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PaymentsModule } from './payments/payments.module';
       isGlobal: true,
     }),
     PaymentsModule,
+    WorkerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
